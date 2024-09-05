c9bd63ddbcc5dd1ec96da23671c14b7e24f013cd
WinWinHtetKyawQualy <winwinhtetkyaw@qualymm.com>
2024-09-05 14:22:16 +0630

    controlling image slider

---
+++ assets/common.js
@@ -0,0 +1,53 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+
+addAnimation();
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+       scroller.setAttribute("data-animated", true); 
+
+        const scrollerInner = scroller.querySelector(".scroller_inner");
+        const scrollerContent = Array.from( scrollerInner.children);
+        const duplicates = [];
+
+        for (let i = 0; i < 3; i++) {
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                duplicatedItem.setAttribute('aria-hidden', true);
+                duplicates.push(duplicatedItem);
+            });
+        }
+
+         // Append all duplicates to scrollerInner
+        duplicates.forEach((duplicatedItem) => {
+            scrollerInner.appendChild(duplicatedItem);
+        });
+    });
+}
+
+
+// 
+// function addAnimation() {
+//     scrollers.forEach((scroller) => {
+//         scroller.setAttribute("data-animated", true);
+
+//         const scrollerInner = scroller.querySelector(".fifty-scroller-inner");
+//         const scrollerContent = Array.from(scrollerInner.children);
+//         const duplicates = [];
+
+//         // Duplicate the scrollerContent two times
+//         for (let i = 0; i < 3; i++) {
+//             scrollerContent.forEach((item) => {
+//                 const duplicatedItem = item.cloneNode(true);
+//                 duplicatedItem.setAttribute("aria-hidden", true);
+//                 duplicates.push(duplicatedItem);
+//             });
+//         }
+
+//         // Append all duplicates to scrollerInner
+//         duplicates.forEach((duplicatedItem) => {
+//             scrollerInner.appendChild(duplicatedItem);
+//         });
+//     });
+// }
